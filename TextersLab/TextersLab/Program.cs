f7291b880853535a14e70eafcee752e2f17f5e3a
Angela <69411622+Kirbachu88@users.noreply.github.com>
2020-09-07 23:31:14 -0700

    Add files via upload

---
+++ TextersLab/TextersLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace TextersLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(55, 25);
+
+            Game.StartGame();
+
+            Console.ReadLine();
+        }
+
+    }
+}
